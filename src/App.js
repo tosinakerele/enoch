ef19fcaf6ea359b484e800cfc440d83d4709eda3
Tosin Akerele <tosin.akerele@doftwerks.com>
2022-11-03 15:09:32 +0100

    first commit for test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React wuth Tosin
+          Learn React with Tosin
         </a>
       </header>
     </div>
